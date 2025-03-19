e16c1f407d6d0b583d031c0a8f7b249d5da42725
Patrick Decat <pdecat@gmail.com>
2025-03-19 10:58:22 +0100

    feat: also untranslate pages from https://cloud.google.com/ containing the ?hl=xx query string

--- firefox/background.js
+++ firefox/background.js
@@ -1,12 +1,27 @@
+// Handle Reddit URLs with 'tl' parameter
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
+  function (details) {
     let url = new URL(details.url);
     if (url.searchParams.has('tl')) {
       url.searchParams.delete('tl');
-      return {redirectUrl: url.toString()};
+      return { redirectUrl: url.toString() };
     }
   },
-  {urls: ["https://www.reddit.com/*"]},
+  { urls: ["https://www.reddit.com/*"] },
+  ["blocking"]
+);
+
+// Handle Google Cloud URLs with 'hl' parameter (any language)
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    let url = new URL(details.url);
+    if (url.searchParams.has('hl')) {
+      url.searchParams.delete('hl');
+      // If there are no more search parameters, remove the '?' from the URL
+      return { redirectUrl: url.toString() };
+    }
+  },
+  { urls: ["https://cloud.google.com/*"] },
   ["blocking"]
 );
 
