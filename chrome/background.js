77a9e31439de053d4ab0c31b83b47db3b8c524ad
Patrick Decat <pdecat@gmail.com>
2025-03-20 14:31:37 +0100

    test: add some debug logs

--- chrome/background.js
+++ chrome/background.js
@@ -1,1 +1,7 @@
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener(
+  (e) => console.log("onRuleMatchedDebug: " + JSON.stringify(e))
+);
+
+console.log("getMatchedRules: " + JSON.stringify(chrome.declarativeNetRequest.getMatchedRules()))
+
 console.log("Background service worker is running.");
